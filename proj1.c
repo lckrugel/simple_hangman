f2ef05dc26abad03ee5ec141c93c58a35726598d
Leonardo Cargnin Krugel <lckrugel@lenovo01.inf.ufsm.br>
2022-07-07 11:59:47 -0300

    ...

--- proj1.c
+++ proj1.c
@@ -11,11 +11,15 @@ void remove_fim_de_linha(char* str);
 
 int tamanho_string(char* str);
 
+void inicia_string(char* str, int tam);
+
 void copia_string(char* alvo, char* origem);
 
-void printa_string(char* str);
+void printa_string(char* str, int espaco);
 
-int testa_letra(char* palavra, char* chute, char* acertos);
+int letra_repetida(char* chute, char* acertos, char* erros);
+
+int letra_certa(char* palavra, char* chute, char* acertos);
 
 int vencedor(char* palavra, char* acertos);
 
@@ -27,22 +31,23 @@ int main(void) {
     char erros[6];
     int vidas = 5;
 
+    inicia_string(acertos, 30);
+    inicia_string(erros, 6);
+
     inicia_jogo();
     while (vidas > 0 && !vencedor(palavra, acertos)) {
         mostra_estado(vidas, palavra, acertos, erros);
         char chute[30];
         le_entrada(chute);
-        /*
-        printa_string(chute);
-        copia_string(acertos, chute);
-        printa_string(acertos);
-        printf("\n");
-        */
         if (tamanho_string(chute) > 1) {
             copia_string(acertos, chute);
             break;
         } else {
-            if (testa_letra(palavra, chute, acertos) == 0) {
+            if (letra_repetida(chute, acertos, erros)) {
+                printf("Voce ja tentou isso.\n");
+                continue;
+            }
+            if (!letra_certa(palavra, chute, acertos) && chute[0] != '\0') {
                 // adiciona ao erro
                 vidas--;
             }
@@ -51,9 +56,16 @@ int main(void) {
 
     printf("\n==== FIM DE JOGO ====\n");
     if (vencedor(palavra, acertos)) {
+        printa_string(acertos, 0);
+        printf("\n");
         printf("GANHOU!\n");
+        printf("=======================\n");
     } else {
         printf("PERDEU.\n");
+        printf("A palavra era: ");
+        printa_string(palavra, 0);
+        printf("\n");
+        printf("=======================\n");
     }
 
     return 0;
@@ -85,11 +97,7 @@ void mostra_estado(int vidas, char* palavra, char* acertos, char* erros)
         i++;
     }
     printf("| Erros: ");
-    i = 0;
-    while (erros[i] != '\0') {
-        printf("%c ", erros[i]);
-        i++;
-    }
+    printa_string(erros, 1);
     printf("\n");
 }
 
@@ -97,8 +105,8 @@ void mostra_estado(int vidas, char* palavra, char* acertos, char* erros)
 /* le o chute do usuario */
 void le_entrada(char* chute)
 {
-    //printf("Chute: ");
-    fgets(chute, sizeof(chute), stdin);
+    printf("Chute: ");
+    fgets(chute, 30, stdin);
 
     remove_fim_de_linha(chute);
 }
@@ -115,9 +123,31 @@ void remove_fim_de_linha(char* str)
 }
 
 
+/* checa se a letra do chute esta contida nos acertos ou erros */
+int letra_repetida(char* chute, char* acertos, char* erros)
+{
+    int i = 0;
+    while (erros[i] != '\0') {
+        if (erros[i] == chute[0]) {
+            return 1;
+        }
+        i++;
+    }
+
+    i = 0;
+    while (acertos[i] != '\0') {
+        if (acertos[i] == chute[0]) {
+            return 1;
+        }
+        i++;
+    }
+    return 0;
+}
+
+
 /* testa se a letra do chute existe na palavra, retorna 1 se existir.
    caso exista, adiciona a letra em acertos */
-int testa_letra(char* palavra, char* chute, char* acertos)
+int letra_certa(char* palavra, char* chute, char* acertos)
 {
     int flag = 0;
     int i = 0;
@@ -141,6 +171,17 @@ int tamanho_string(char* str)
 }
 
 
+/* preenche o vetor str de vetor tam com ' ' e o finaliza com '\0' */
+void inicia_string(char* str, int tam)
+{
+    int i = 0;
+    for (i = 0; i < tam-1; i++) {
+        str[i] = ' ';
+    }
+    str[i] = '\0';
+}
+
+
 /* copia a segunda string para a primeira */
 void copia_string(char* alvo, char* origem)
 {
@@ -153,25 +194,25 @@ void copia_string(char* alvo, char* origem)
 }
 
 
-void printa_string(char* str)
+/* printa a string str, caso espaco seja true (1) vai adicionar um espaco
+   entre os caracteres. */
+void printa_string(char* str, int espaco)
 {
     int i = 0;
     while (str[i] != '\0') {
         printf("%c", str[i]);
+        if(espaco) printf(" ");
         i++;
     }
-    printf("   ");
+    printf("");
 }
 
 
 /* verifica se os acertos correspondem a palavra. retorna 1 quando vencedor. */
 int vencedor(char* palavra, char* acertos)
 {
-    if (tamanho_string(palavra) != tamanho_string(acertos)) return 0;
-
-    printf("\nOlá\n");
     int i = 0;
-    while (palavra[i] != '\0'){
+    while (palavra[i] != '\0') {
         if (acertos[i] != palavra[i]) return 0;
         i++;
     }
